97694ab044c18d431ef9955e3bc9e959f2913f70
tinoaxmskp <151659087+tinoaxmskp@users.noreply.github.com>
2024-10-27 22:23:04 +0100

    Initial commit

---
+++ app/src/main/java/com/example/loginapp/HomePage.kt
@@ -0,0 +1,21 @@
+package com.example.loginapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.loginapp.R.id.main
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.home_page)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
